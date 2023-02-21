8c58d6c2b4c22b57c4a6a2a2f593e5e869a07a05
MindIssawara <kmind46@gmail.com>
2023-02-21 15:46:17 +0700

    completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,22 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+    if(R2.x-R1.x > R1.w || R2.y-R1.y>R2.h ) return 0;
+    
+    double max,min,w,h;
+    
+    if(R1.x>=R2.x) max=R1.x;
+        else max=R2.x;
+    if(R1.x+R1.w>=R2.x+R2.w) min=R2.x+R2.w;
+        else min=R1.x+R1.w;
+    w= min-max;
+    
+    if(R1.y<=R2.y) min=R1.y;
+        else min=R2.y;
+    if(R1.y-R1.h>=R2.y-R2.h) max=R1.y-R1.h;
+        else max=R2.y-R2.h;
+    h= min-max;
+    
+    return w*h;
 }
